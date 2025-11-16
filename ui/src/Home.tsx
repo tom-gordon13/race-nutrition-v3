bd4392494638100abb64c7a00dd14d6e9b9b46e0
tom-gordon13 <tombcgordon@gmail.com>
2025-11-16 07:36:22 -0700

    unused import

--- ui/src/Home.tsx
+++ ui/src/Home.tsx
@@ -1,4 +1,3 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import { Routes, Route } from "react-router-dom";
 import Nutrients from "./Nutrients";
 import CreateFoodItem from "./CreateFoodItem";
