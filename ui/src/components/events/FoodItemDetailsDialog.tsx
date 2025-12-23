851edfdec71a5ff17603dafffb1118ffd8b4e6d0
tom-gordon13 <tombcgordon@gmail.com>
2025-12-23 18:14:09 -0500

    fix build error

--- ui/src/components/events/FoodItemDetailsDialog.tsx
+++ ui/src/components/events/FoodItemDetailsDialog.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Dialog } from 'primereact/dialog';
-import { Tag } from 'primereact/tag';
 import './FoodItemDetailsDialog.css';
 
 interface FoodItemNutrient {
